aa961650107868a6e290cd6b248aa83d39531717
hardikbachhan <50964127+hardikbachhan@users.noreply.github.com>
2022-03-24 00:23:39 +0530

    complete Register.jsx to register a new user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import DisplayPost from "./pages/display post/DisplayPost";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       {/* <DisplayPost /> */}
       {/* <Write /> */}
       {/* <Settings /> */}
-      <Login />
+      {/* <Login /> */}
+      <Register />
     </>
   );
 }
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,36 @@
+import "./register.css";
+
+function Register() {
+  return (
+    <div className="register">
+      <span className="registerTitle">Register</span>
+      <form className="registerForm">
+        <label>Username</label>
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your username..."
+          required
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          className="registerInput"
+          placeholder="Enter your email..."
+          required
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+          required
+        />
+        <button className="registerButton">Register</button>
+      </form>
+      <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
+
+export default Register;
